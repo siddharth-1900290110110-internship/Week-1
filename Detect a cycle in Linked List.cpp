fac44d851830e3cccb95a3d907a761a1f1db5a44
siddharth-1900290110110-internship <108752752+siddharth-1900290110110-internship@users.noreply.github.com>
2022-07-14 22:10:25 +0530

    Create Detect a cycle in Linked List.cpp

---
+++ Detect a cycle in Linked List.cpp
@@ -0,0 +1,14 @@
+bool cycleDetect(node* head) {
+  
+    unordered_set<node*> hashTable;
+  
+    while(head != NULL) {
+      
+        if(hashTable.find(head) != hashTable.end()) return true;
+      
+        hashTable.insert(head);
+      
+        head = head->next;
+    }
+    return false;
+}
